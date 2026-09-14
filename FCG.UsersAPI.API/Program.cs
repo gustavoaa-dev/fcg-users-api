9c27722aca50c5113814499716d2d0718bbcf4ea
Gustavo A. Araujo <126591395+gustavoaa-dev@users.noreply.github.com>
2026-09-13 23:17:59 -0300

    feat: expoe metricas prometheus e health check

--- FCG.UsersAPI.API/Program.cs
+++ FCG.UsersAPI.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
+using Prometheus;
 using System.Security.Claims;
 using System.Text;
 
@@ -98,7 +99,13 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<AuthService>();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
+
+// Metricas HTTP: registradas antes dos demais middlewares para contar tambem
+// as respostas geradas por erro/autenticacao (401, 404, 500).
+app.UseHttpMetrics();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -118,6 +125,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+app.MapMetrics();
+app.MapHealthChecks("/health");
 
 app.Run();
 
